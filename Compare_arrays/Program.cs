57133fdf525694d11faf6c737a80f5e608ef662f
martinmp <martomsl@gmail.com>
2023-08-11 14:59:56 +0300

    compare arrays

---
+++ Compare_arrays/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace MyProject
+{
+    class Program
+    {
+        static int[] InitializeArray(int n)
+        {
+            int[] arr = new int[n];
+
+            for (int i = 0; i < n; i++)
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+
+            return arr;
+        }
+        static string CompareElementsOfTwoArrays(int[] arrayOne, int[] arrayTwo, int size)
+        {
+            string output = "Equal";
+
+            for (int i = 0; i < size; i++)
+            {
+                if (arrayOne[i] != arrayTwo[i])
+                {
+                    output = "Not equal";
+                    break;
+                }
+            }
+
+            return output;
+        }
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine()!);
+
+            int[] arrayOne = InitializeArray(n);
+            int[] arrayTwo = InitializeArray(n);
+
+            Console.WriteLine(CompareElementsOfTwoArrays(arrayOne, arrayTwo, n));
+        }
+    }
+}
+
+/*
+Description
+Write a program that reads two integer arrays of size N from the console and compares them element by element.
+
+Input
+On the first line you will receive the number N
+On the next N lines the numbers of the first array will be given
+On the next N lines the numbers of the second array will be given
+
+Output
+Print Equal if the two arrays are the same and Not equal if they are not
+
+Constraints
+1 <= N <= 20
+N will always be a valid integer number
+Time limit: 0.1s
+Memory limit: 16MB
+*/
---
+++ Compare_char_arrays/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace MyProject
+{
+    class Program
+    {
+        static char[] InitializeCharArray(string str)
+        {
+            char[] arr = new char[str.Length];
+
+            for (int i = 0; i < str.Length; i++)
+                arr[i] = str[i];
+
+            return arr;
+        }
+        static string LexicographicCharArrayComparer(char[] arrayOne, char[] arrayTwo)
+        {
+            string output = "=";
+            int size = Math.Min(arrayOne.Length, arrayTwo.Length);
+
+            if (arrayOne.Length > arrayTwo.Length)
+            {
+                output = ">";
+            }
+            if (arrayOne.Length < arrayTwo.Length)
+            {
+                output = "<";
+            }
+            if (arrayOne.Length == arrayTwo.Length)
+            {
+                bool resolved = false;
+                for (int i = 0; i < size; i++)
+                {
+                    if (arrayOne[i] < arrayTwo[i])
+                    {
+                        output = "<";
+                        resolved = true;
+                        break;
+                    }
+                    if (arrayOne[i] > arrayTwo[i])
+                    {
+                        output = ">";
+                        resolved = true;
+                        break;
+                    }
+                }
+                if (!resolved)
+                {
+                    output = "=";
+                }
+            }
+
+            return output;
+        }
+        static void Main(string[] args)
+        {
+            string inputString1 = Console.ReadLine()!;
+            string inputString2 = Console.ReadLine()!;
+
+            char[] arrayOne = InitializeCharArray(inputString1);
+            char[] arrayTwo = InitializeCharArray(inputString2);
+
+            Console.WriteLine(LexicographicCharArrayComparer(arrayOne, arrayTwo));
+        }
+    }
+}
+
+/*
+Description
+Write a program that compares two char arrays lexicographically (letter by letter).
+
+Input
+On the first line you will receive the first char array as a string
+On the second line you will receive the second char array as a string
+
+Output
+Print < if the first array is lexicographically smaller
+Print > if the second array is lexicographically smaller
+Print = if the arrays are equal
+
+Constraints
+1 <= size of arrays <= 128
+Time limit: 0.1s
+Memory limit: 16MB
+*/
